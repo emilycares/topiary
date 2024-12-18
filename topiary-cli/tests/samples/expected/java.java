12e8ad25ecf46b4f778cc535f58f90b7070a1746
emilycares <emilydoescare@gmail.com>
2024-12-18 11:21:41 +0100

    Fix binary_expression and variable_declarator

--- topiary-cli/tests/samples/expected/java.java
+++ topiary-cli/tests/samples/expected/java.java
@@ -5,13 +5,17 @@ import java.lang.String;
 
 import java.lang.String;
 
+@Huh
 public class Test {
 
   // asd
   private String a
   private static String a;
 
+  @Huh
+  @HuhProp("")
   public String test() {
+    String b = "a" + "c" > 2;
     return "a";
   }
 }
