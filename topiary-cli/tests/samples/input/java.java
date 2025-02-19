80a2809378dc982335e6160b1ed0fbf96f7dd186
emilycares <emilydoescare@gmail.com>
2025-02-19 14:06:05 +0100

    Impove query

--- topiary-cli/tests/samples/input/java.java
+++ topiary-cli/tests/samples/input/java.java
@@ -1,21 +1,37 @@
 package topiary;
 
-import java.lang.String;
-import java.lang.String;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
-import java.lang.String;
+import java.util.List;
 
-@Huh
-public class Test {
+@Path("/test")
+public class GreetingResource {
 
-    // asd
-    private String a
-    private static String a ;
+  private class HIhi {}
 
-    @Huh
-    @HuhProp("")
-    public String test() {
-        String b="a"+"c">2;
-        return "a";
+  private static Long other;
+
+  @GET
+  @Produces(MediaType.TEXT_HTML)
+  public String hellos(String a) {
+    String local = other.toString();
+    List<String> names = List.of("a", "b");
+    for(int i = 0; i < 5;i++) {
+      for(String name : names) {
+        names
+            .stream().map((n) -> {
+          return n + "_";
+        });
+      }
     }
+
+    new Variable().hello();
+    hello.data("");
+    local.concat("");
+
+    return "asdf";
+  }
 }
--- topiary-queries/queries/java.scm
+++ topiary-queries/queries/java.scm
@@ -1,7 +1,8 @@
 ; Allow blank line before
 [
- (import_declaration)
- (class_declaration)
+  (import_declaration)
+  (class_declaration)
+  (expression_statement)
 ] @allow_blank_line_before
 
 (package_declaration "package" @append_space)
@@ -20,37 +21,84 @@
   .
 )
 
-(method_declaration (block
-  .
-  "{" @append_hardline @append_indent_start @prepend_space
-  (_)
-  "}" @prepend_hardline @prepend_indent_end @append_hardline
-  .
-))
+(method_declaration
+  (block
+    .
+    "{" @append_hardline @append_indent_start @prepend_space
+    (_)
+    "}" @prepend_hardline @prepend_indent_end @append_hardline
+    .
+  )
+)
+
+(for_statement
+  (block
+    .
+    "{" @append_hardline @append_indent_start @prepend_space
+    (_)
+    "}" @prepend_hardline @prepend_indent_end @append_hardline
+    .
+  )
+)
+
+(enhanced_for_statement
+  (block
+    .
+    "{" @append_hardline @append_indent_start @prepend_space
+    (_)
+    "}" @prepend_hardline @prepend_indent_end @append_hardline
+    .
+  )
+)
+
+(lambda_expression
+  (block
+    .
+    "{" @append_hardline @append_indent_start @prepend_space
+    (_)
+    "}" @prepend_hardline @prepend_indent_end
+    .
+  )
+)
+(lambda_expression "->" @prepend_space @append_space)
 
 [
- ";"
- (marker_annotation)
- (annotation)
-] @append_hardline
+  (field_declaration ";")
+  (expression_statement ";")
+  (marker_annotation)
+  (annotation)
+] @append_spaced_softline
 
+(_
+  (local_variable_declaration ";") @append_spaced_scoped_softline
+  (#scope_id! "varsemi")
+) @prepend_begin_scope @append_end_scope
+; No newline after local_variable_declaration in for_statement
+(for_statement
+  init: (local_variable_declaration
+    ";"
+  ) @prepend_begin_scope @append_end_scope @append_empty_scoped_softline
+  (#scope_id! "varsemi")
+)
 [
- (return_statement)
- (import_declaration)
- (field_declaration)
+  (return_statement)
+  (import_declaration)
+  (field_declaration)
 ] @prepend_hardline
 
-
 [
- (import_declaration)
- (class_declaration)
- (return_statement)
- (method_declaration)
- (field_declaration)
- (line_comment)
- (block_comment)
+  (import_declaration)
+  (class_declaration)
+  (return_statement)
+  (method_declaration)
+  (field_declaration)
+  (line_comment)
+  (block_comment)
 ] @allow_blank_line_before
 
+; Format arguments
+(argument_list "," @append_space)
+(formal_parameters "," @append_space)
 
 ; Input softlines before and after all comments. This means that the input
 ; decides if a comment should have line breaks before or after. A line comment
@@ -61,40 +109,75 @@
 ] @prepend_input_softline
 
 [
- "static"
- (type_identifier)
+  "static"
 ] @prepend_space @append_space
 
-
+(method_declaration
+  type: [
+    (type_identifier)
+    (generic_type)
+    (integral_type)
+  ] @prepend_space @append_space
+)
+(field_declaration
+  type: [
+    (type_identifier)
+    (generic_type)
+    (integral_type)
+  ] @prepend_space @append_space
+)
+(local_variable_declaration
+  type: [
+    (type_identifier)
+    (generic_type)
+    (integral_type)
+  ] @append_space
+)
+(formal_parameter
+  type: [
+    (type_identifier)
+    (generic_type)
+    (integral_type)
+  ] @append_space
+)
+(enhanced_for_statement
+  type: [
+    (type_identifier)
+    (generic_type)
+    (integral_type)
+  ] @append_space
+)
+(for_statement ";" @append_antispace)
+(enhanced_for_statement ":" @prepend_space @append_space)
 (variable_declarator "=" @prepend_space @append_space)
 
 (binary_expression
   [
-   "+"
-   "-"
-   "*"
-   "/"
-   "%"
-   "="
-   "+="
-   "-="
-   "*="
-   "/="
-   "%="
-   "&="
-   "|="
-   "^="
-   ">>="
-   "<<="
-   "=="
-   "!="
-   ">"
-   "<"
-   ">="
-   "<="
-   "&&"
-   "||"
-   "!"
+    "+"
+    "-"
+    "*"
+    "/"
+    "%"
+    "="
+    "+="
+    "-="
+    "*="
+    "/="
+    "%="
+    "&="
+    "|="
+    "^="
+    ">>="
+    "<<="
+    "=="
+    "!="
+    ">"
+    "<"
+    ">="
+    "<="
+    "&&"
+    "||"
+    "!"
   ] @prepend_space @append_space
 )
 
@@ -106,3 +189,4 @@
   "."
 ] @prepend_antispace
 
+(object_creation_expression "new" @append_space)
